5948146d495c6d2e27c2e2922683d1e6c55f9b74
Shashank B R <shashankbr21494@gmail.com>
2022-11-24 16:55:34 +0530

    changes

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,12 +1,14 @@
 import ProfileCard from "./ProfileCard";
-
+import AlexaImage from "./images/alexa.png";
+import SiriImage from "./images/siri.png";
+import CortanaImage from "./images/cortana.png";
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
     </div>
   );
 }
